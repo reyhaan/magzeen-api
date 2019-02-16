872cc1fdd693c52a1f755d3e84a8c9dedd5c1041
Zeeshan <zeeshan@Zeeshans-MacBook-Pro.local>
2019-02-17 01:54:20 +0530

    refactor and add more stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -16,6 +16,14 @@ items = [
 ]
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -32,8 +40,15 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'item deleted!'}
 
-    def put(self):
-        pass
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, data: data['desc']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 api.add_resource(Item, '/item/<string:name>')
 
