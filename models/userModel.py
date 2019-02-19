19dc9947f326ac3def3ba037ba810f97fb2d2de6
Zeeshan <zeeshan@Zeeshans-MacBook-Pro.local>
2019-02-19 17:01:49 +0530

    add more methods to UserModel

--- models/userModel.py
+++ models/userModel.py
@@ -27,28 +27,91 @@ class UserModel:
         self.company_name = company_name
 
     @classmethod
-    def find_user_by_id(cls):
-        return 'a user'
+    def find_user_by_id(cls, user_id):
+        sql = """SELECT * FROM users WHERE user_id=%s"""
+        conn = None
+        user = None
+        try:
+            conn, cur = db.connect()
+            cur.execute(sql, (user_id,))
+            user = cur.fetchone()
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return user
 
     @classmethod
-    def find_user_by_name(cls):
-        return 'a user'
+    def find_user_by_email(cls, email):
+        sql = """SELECT * FROM users WHERE email=%s"""
+        conn = None
+        user = None
+        try:
+            conn, cur = db.connect()
+            cur.execute(sql, (email,))
+            user = cur.fetchone()
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return user
 
     @classmethod
-    def delete_user(cls):
-        return 'user deleted'
+    def delete_user(cls, email):
+        sql = """DELETE FROM users WHERE email=%s"""
+        conn = None
+        user = None
+        try:
+            conn, cur = db.connect()
+            cur.execute(sql, (email,))
+            user = cur.fetchone()
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return user
 
     @classmethod
     def update_user(cls):
         pass
 
     @classmethod
-    def verify_user(cls, username, password):
-        pass
+    def verify_user(cls, email, password):
+        sql = """SELECT * FROM users WHERE email=%s AND password=%s"""
+        conn = None
+        user = None
+        count = 0
+        password = check_password_hash(password)
+        try:
+            conn, cur = db.connect()
+            cur.execute(sql, (email, password))
+            user = cur.fetchone()
+            if user is not None:
+                count = len(user)
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return count
 
     @classmethod
     def check_if_user_exists(cls, email):
-        """ insert a new vendor into the vendors table """
         sql = """SELECT * FROM users WHERE email=%s"""
         conn = None
         count = 0
@@ -72,7 +135,6 @@ class UserModel:
     @classmethod
     def add_user(cls, email, password):
         password = generate_password_hash(password)
-        """ insert a new vendor into the vendors table """
         sql = """INSERT INTO users(email, password)
                 VALUES(%s, %s) RETURNING (user_id, email);"""
         conn = None
