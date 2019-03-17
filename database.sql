b8bd08f37735bd6a06de1d720bf6d80862d38523
Rehaan <frankfill2007@gmail.com>
2019-03-16 22:17:44 -0400

    make changes to database file

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS users, teams, user_team_junction, posts, comments, reactions, sections, user_access_level CASCADE;
+
 CREATE TABLE users (
-	user_id serial PRIMARY KEY,
+	user_id SERIAL PRIMARY KEY,
     first_name VARCHAR,
 	last_name VARCHAR,
     password VARCHAR NOT NULL,
@@ -7,34 +9,59 @@ CREATE TABLE users (
     created_at DATE,
 	updated_at DATE,
     type VARCHAR,
-    domain_name VARCHAR,
-	team_id INT,
-    FOREIGN KEY (team_id) REFERENCES teams (team_id)
+    domain_name VARCHAR
 );
 
 CREATE TABLE teams (
-	team_id serial PRIMARY_KEY,
+	team_id SERIAL PRIMARY KEY,
 	team_name VARCHAR,
 	team_slug VARCHAR,
 	team_domain VARCHAR
 );
 
-CREATE TABLE user_access_level (
+CREATE TABLE user_team_junction (
+	user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+	team_id INTEGER REFERENCES teams(team_id) ON DELETE CASCADE,
+	CONSTRAINT junction_id PRIMARY KEY (user_id, team_id)
+);
 
+CREATE TABLE user_access_level (
+	user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+	team_id INTEGER REFERENCES teams(team_id) ON DELETE CASCADE,
+	access_level VARCHAR
 );
 
 CREATE TABLE comments (
-
+	comment_id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(user_id),
+	content VARCHAR,
+	created_at DATE,
+	updated_at DATE
 );
 
 CREATE TABLE posts (
-
+	post_id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(user_id),
+	title VARCHAR,
+	body VARCHAR,
+	mentions VARCHAR,
+	tags VARCHAR,
+	created_at DATE,
+	updated_at DATE
 );
 
 CREATE TABLE reactions (
-
+	reaction_id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(user_id),
+	reaction VARCHAR,
+	created_at DATE,
+	updated_at DATE
 );
 
 CREATE TABLE sections (
-	
+	section_id SERIAL PRIMARY KEY,
+	team_id INTEGER REFERENCES teams(team_id),
+	name VARCHAR,
+	created_at DATE,
+	updated_at DATE
 );
